8778eb8a0c68e3da00f59eb053e743b2eb4ddcf6
guime7000 <tguimezanes@gmail.com>
2023-08-06 17:21:20 +0200

    End of TAP

--- mini-sql-guime7000/dist/exit.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.myExit = void 0;
-const myExit = (params) => {
-    process.exit(+params);
-};
-exports.myExit = myExit;
